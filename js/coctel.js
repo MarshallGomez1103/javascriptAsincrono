e0dac422d91241199379bcfaad2a2eb28db66372
Marshall Gomez <thomasgomez1103@hotmail.com>
2025-08-28 23:50:49 -0500

    Add random cocktail feature to coctel.html
    Introduces a 'Coctel Aleatorio' button and a section to display a random cocktail in coctel.html. Updates coctel.js to handle the button click, reload the page, and dynamically render cocktail details including name, image, ingredients, and instructions. Adds responsive styling for images on small screens. Removes the random cocktail button and script from index.html to avoid redundancy.

--- js/coctel.js
+++ js/coctel.js
@@ -6,6 +6,12 @@ const input = document.getElementById("input-coctel");
 const estado = document.getElementById("estado");
 const resultados = document.getElementById("resultados");
 
+//creo la contante boton aleatorio para que cada vez que se oprima se recargue la pagina
+const btnAleatorio = document.querySelector(".aleatorio");
+btnAleatorio.addEventListener("click", () =>{
+    location.reload();
+})
+
 // verificar que la persona haya mandado un input texto y que la pápgina no se recargue cada vez que se cargue
 /*
 
@@ -52,6 +58,39 @@ obtenerUnCoctelRandom();
 //elemento que es el 0, ademas podemos mostrar solo el nombre
 //console.log(coctel.drinks[0].strDrink);
 //y hay mas cosas para mostrar
-function mostrarCoctelRandom(coctel){
-    console.log(coctel.drinks[0]);
+function mostrarCoctelRandom(cocktail){
+    console.log(cocktail.drinks[0]);
+
+    let drinkSection = document.querySelector('.drinkSection');
+
+    //Esto pone el nombre del coctel
+    let drinkName = document.createElement("h2")
+    drinkName.innerHTML = `<br>` + cocktail.drinks[0].strDrink + `, ID(${cocktail.drinks[0].idDrink})` + `<br> Category: ${cocktail.drinks[0].strCategory}`;
+
+    drinkSection.appendChild(drinkName);
+
+
+    //Esto toma y pone la imagen del coctel
+    let img = document.createElement("img")
+    img.src = cocktail.drinks[0].strDrinkThumb;
+
+    drinkSection.appendChild(img);
+
+    //Esto itera por los 15 posibles ingredientes y los agrega
+    for (let i = 1; i < 16; i++) {
+
+        if (cocktail.drinks[0][`strIngredient${i}`] === null || cocktail.drinks[0][`strIngredient${i}`] === ""){
+            break;
+        }
+        let ingredient = document.createElement("ingredients")
+        ingredient.innerHTML = `<br>` + cocktail.drinks[0][`strIngredient${i}`] + ": " + cocktail.drinks[0][`strMeasure${i}`];
+
+        drinkSection.appendChild(ingredient);
+    }
+
+    //esto agrega la preparacion
+    let card = document.createElement("ons-card");
+    card.innerHTML = `<br>` + `<br>` + cocktail.drinks[0].strInstructions;
+
+    drinkSection.appendChild(card)
 }
